05eb97e5f71d8f165b97c3e05c85ea4f2f06a6bf
Eduardo Morgado <edu.dado99@gmail.com>
2019-04-30 12:38:52 +0100

    Update Main.java

--- Version2-GUI/src/Main.java
+++ Version2-GUI/src/Main.java
@@ -1,7 +1,5 @@
 public class Main{
     public static void main(String[] args){
-        System.out.println(args[0]);
-        System.out.println(args.length);
         if(args.length != 1)
             new TreeBuilder(false,null);
         else
